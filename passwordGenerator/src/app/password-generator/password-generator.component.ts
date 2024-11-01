10223fe79abb0d09f51bbd8bfa106f172f0e8b6f
Emil Wilson <emilu551e@gmail.com>
2024-11-01 15:51:58 +0530

    completed

---
+++ passwordGenerator/src/app/password-generator/password-generator.component.ts
@@ -0,0 +1,72 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-password-generator',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './password-generator.component.html',
+  styleUrl: './password-generator.component.css'
+})
+export class PasswordGeneratorComponent {
+  passwordLength: number = 8;
+  includeUppercase: boolean = true;
+  includeLowercase: boolean = true;
+  includeNumbers: boolean = true;
+  includeSpecialChars: boolean = true;
+  generatedPassword: string = '';
+  errorMessage: string = '';
+  savedPasswords: string[] = [];
+
+  constructor(private clipboard: Clipboard) {}
+
+  generatePassword(): void {
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const numberChars = '0123456789';
+    const specialChars = '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+    
+    let charSet = '';
+    this.generatedPassword = '';
+    this.errorMessage = '';
+
+    if (this.includeUppercase) charSet += uppercaseChars;
+    if (this.includeLowercase) charSet += lowercaseChars;
+    if (this.includeNumbers) charSet += numberChars;
+    if (this.includeSpecialChars) charSet += specialChars;
+
+    if (!charSet) {
+      this.errorMessage = 'Please select at least one character type!';
+      return;
+    }
+
+    for (let i = 0; i < this.passwordLength; i++) {
+      const randomIndex = Math.floor(Math.random() * charSet.length);
+      this.generatedPassword += charSet[randomIndex];
+    }
+  }
+
+  copyToClipboard(): void {
+    if (this.generatedPassword) {
+      this.clipboard.copy(this.generatedPassword);
+      alert('Password copied to clipboard!');
+    }
+  }
+
+  copy(password:string) {
+    if (password) {
+      this.clipboard.copy(password);
+      alert('Password copied to clipboard!');
+    }
+  }
+
+  savePassword(): void {
+    if (this.generatedPassword) {
+      this.savedPasswords.push(this.generatedPassword);
+      this.generatedPassword = ''; // Clear after saving
+      alert('Password saved successfully!');
+    }
+  }
+}
